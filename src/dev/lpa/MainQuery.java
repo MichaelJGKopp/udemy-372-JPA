4bb25f91880c8c1d120220f27ff4994ef98ebfbb
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-11-04 17:58:08 +0100

    user CriteriaBuilder for query

--- src/dev/lpa/MainQuery.java
+++ src/dev/lpa/MainQuery.java
@@ -5,8 +5,13 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.Tuple;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 import java.util.List;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainQuery {
@@ -23,6 +28,19 @@ public class MainQuery {
       artists = getArtistKPQL(em, "%Greatest Hits%");
       artists.forEach(System.out::println);
       
+      System.out.println("----------------------------------------");
+      Stream<Artist> sartists = getArtistsBuilder(em, "");
+      var map = sartists
+                  .limit(10)
+                  .collect(Collectors.toMap(
+                    Artist::getArtistName,
+                    (a) -> a.getAlbums().size(),
+                    Integer::sum,
+                    TreeMap::new
+                  ));
+      
+      map.forEach((k, v) -> System.out.println(k + " : " + v));
+
 //      var names = getArtistNames(em, "%Stev%");
 //      names
 //        .map(a -> new Artist(
@@ -36,7 +54,7 @@ public class MainQuery {
   }
   
   private static List<Artist> getArtistKPQL(EntityManager em, String matchedValue) {
-    
+
 //    String jpql = "SELECT a FROM Artist a";
 //    String jpql = "SELECT a FROM Artist a WHERE a.artistName LIKE ?1";  // random param. name
     String jpql = "SELECT a FROM Artist a JOIN albums album " +
@@ -49,7 +67,7 @@ public class MainQuery {
   }
   
   private static Stream<Tuple> getArtistNames(EntityManager em, String matchedValue) {
-
+    
     String jpql = "SELECT a.artistId id, a.artistName as name FROM Artist a " +
                     "WHERE a.artistName LIKE ?1";  // random param. name
     var query = em.createQuery(jpql, Tuple.class);
@@ -57,4 +75,12 @@ public class MainQuery {
     return query.getResultStream();
   }
   
+  private static Stream<Artist> getArtistsBuilder(EntityManager em, String matchedValue) {
+    
+    CriteriaBuilder builder = em.getCriteriaBuilder();
+    CriteriaQuery<Artist> criteriaQuery = builder.createQuery(Artist.class);
+    Root<Artist> root = criteriaQuery.from(Artist.class);
+    criteriaQuery.select(root);
+    return em.createQuery(criteriaQuery).getResultStream();
+  }
 }
